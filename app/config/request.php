db136cfad8649ab6dacf0d90cfdc2262f354f271
Tom Ellis <tellishtc@gmail.com>
2015-01-03 12:06:22 +0000

    Update request config

--- app/config/request.php
+++ app/config/request.php
@@ -4,13 +4,13 @@ return array(
     // Used by the URL Generator via the RequestContext class
     #'base_url' => '/app_dev.php',
 
-    'host'     => 'tomahawkphp.com.local',
+    'host'     => 'tomahawkphp.com',
 
     'asset_host' => '',
 
     'scheme'   => 'http',
 
-    'http_port'  => 8182,
+    'http_port'  => 80,
     'https_port' => 443,
 
     'ssl'       => true, //Used when generating URLs to override https
