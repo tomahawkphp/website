64869eb1d0107fda301d133db9b19c1ac7f371d6
Tom Ellis <tellishtc@gmail.com>
2015-01-09 19:28:59 +0000

    Start of CLI docs

--- src/TomahawkPHP/Bundle/WebBundle/Resources/views/Docs/Components/cli.php
+++ src/TomahawkPHP/Bundle/WebBundle/Resources/views/Docs/Components/cli.php
@@ -2,10 +2,97 @@
 
 <?php $view['blocks']->start('content') ?>
 
+    <ol class="breadcrumb">
+        <li>
+            <a href="<?php echo $view['url']->route('docs.home') ?>">Documentation</a>
+        </li>
+        <li class="active">Hatchet CLI</li>
+    </ol>
+
+    <h1>
+        Hatchet CLI
+    </h1>
+
     <p>
-        Coming soon...
+        The Hatchet Command Line Interface is build around the Symfony2 Console Component. You can read more about it
+        <a class="links" href="http://symfony.com/doc/current/components/console/introduction.html" target="_blank">here</a>.
     </p>
 
+
+    <h3>
+        Using the CLI
+    </h3>
+
+    <p>
+        Make sure you've changed to the directory of the project in Terminal/Command Prompt.
+    </p>
+
+    <p>
+        You can run the following to see what commands are available:
+    </p>
+
+<pre>
+<code>app/hatchet</code>
+</pre>
+
+    <h3>
+        Writing your own commands
+    </h3>
+
+    <p>
+        This sections assumes you already have a Bundle created. To learn more about Bundles click
+        <a class="links" href="<?php echo $view['url']->route('docs.components.bundles') ?>" target="_blank">here</a>.
+    </p>
+
+    <p>
+        All commands must follow the following rules:
+    </p>
+
+    <ul>
+        <li>The class name must end with <code>Command.php</code></li>
+        <li>All commands must be in a Command folder within your Bundle</li>
+    </ul>
+
+    <p>
+        Below is an example of a command:
+    </p>
+
+<div>
+<script data-style="application/x-httpd-php" type="x-code-example">
+&lt;?php
+
+namespace MyCompany\MyBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MyCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('hello')
+            ->setDescription('Say Hello.');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello');
+    }
+
+}
+</script>
+</div>
+
+
+    <p>
+        You can run the above command by running the following:
+    </p>
+
+<pre>
+<code>app/hatchet hello</code>
+</pre>
 
 <div class="push-down-20"></div>
 
