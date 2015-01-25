bfcf94bf57d65c3946851f9c28b6a44d3a3a9e98
Tom Ellis <tellishtc@gmail.com>
2015-01-25 15:10:54 +0000

    Start of Service Provider section in DI docs

--- src/TomahawkPHP/Bundle/WebBundle/Resources/views/Docs/Components/di.php
+++ src/TomahawkPHP/Bundle/WebBundle/Resources/views/Docs/Components/di.php
@@ -14,9 +14,104 @@
     </h1>
 
     <p>
-        The Dependency Injection component allows you to define the way objects are constructed in your application.
+        The Dependency Injection (DI) component allows you to define the way objects are constructed in your application.
     </p>
 
+    <h3>
+        Adding single services to the DI Container.
+    </h3>
+
+    <p>
+        The easiest way to add services is to do it in a Bundle.
+        You can read more about them
+        <a class="link-me" href="<?php echo $view['url']->route('docs.components.bundles') ?>">here</a>.
+    </p>
+
+    <p>
+        Below is an example of adding a service to the DI Container:
+    </p>
+
+<div>
+<script data-style="application/x-httpd-php" type="x-code-example">
+&lt;?php
+
+namespace MyCompany\Bundle\WebBundle;
+
+use Tomahawk\HttpKernel\Bundle\Bundle;
+
+class WebBundle extends Bundle
+{
+    public function boot()
+    {
+        $this->container->set('email', new Mailer());
+    }
+}
+</script>
+</div>
+
+
+    <p>
+        What if your service required another service as a parameter,
+        well you just need to pass the second parameter as a closure:
+    </p>
+
+<div>
+<script data-style="application/x-httpd-php" type="x-code-example">
+&lt;?php
+
+namespace MyCompany\Bundle\WebBundle;
+
+use Tomahawk\HttpKernel\Bundle\Bundle;
+use Tomahawk\DI\ContainerInterface;
+
+class WebBundle extends Bundle
+{
+    public function boot()
+    {
+        $this->container->set('email', function(ContainerInterface $c) {
+            return new Mailer($c->get('transport'));
+        });
+    }
+}
+</script>
+</div>
+
+
+    <p>
+        The above examples will return the same object each time,
+        if you wanted to return a different object every time you got it out of the DI container you can do:
+    </p>
+
+
+<div>
+<script data-style="application/x-httpd-php" type="x-code-example">
+&lt;?php
+
+namespace MyCompany\Bundle\WebBundle;
+
+use Tomahawk\HttpKernel\Bundle\Bundle;
+use Tomahawk\DI\ContainerInterface;
+
+class WebBundle extends Bundle
+{
+    public function boot()
+    {
+        $this->container->set('email', $this->container->factory(function(ContainerInterface $c) {
+            return new Mailer($c->get('transport'));
+        }));
+    }
+}
+</script>
+</div>
+
+
+    <h3>
+        Creating a Service Provider
+    </h3>
+
+    <p>
+
+    </p>
 
 <div class="push-down-20"></div>
 
