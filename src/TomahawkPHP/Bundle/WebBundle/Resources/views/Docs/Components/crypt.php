8b132ee83190d580d10c9d7d4d8b1d570776f829
Tom Ellis <tellishtc@gmail.com>
2015-01-25 15:10:28 +0000

    Start of Crypt docs

--- src/TomahawkPHP/Bundle/WebBundle/Resources/views/Docs/Components/crypt.php
+++ src/TomahawkPHP/Bundle/WebBundle/Resources/views/Docs/Components/crypt.php
@@ -2,8 +2,29 @@
 
 <?php $view['blocks']->start('content') ?>
 
+    <ol class="breadcrumb">
+        <li>
+            <a href="<?php echo $view['url']->route('docs.home') ?>">Documentation</a>
+        </li>
+        <li class="active">Encryption</li>
+    </ol>
+
+    <h1>
+        Encryption
+    </h1>
+
     <p>
-        Coming soon...
+        The Encryption component is used to encrypt and decrypt strings.
+    </p>
+
+    <p>
+        The easiest way to use the Encryption component is to make sure your Controllers extend
+        <code>Tomahawk\Routing\Controller</code>. You then have access to it through <code>$this->crypt</code>.
+    </p>
+
+    <p>
+        Otherwise just add the following parameter to the construct method of your
+        Controller <code>Tomahawk\Encryption\CryptInterface</code> and it will get injected in through the Service Container.
     </p>
 
 
