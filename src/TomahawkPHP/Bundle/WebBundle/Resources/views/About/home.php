387b32eba88adde8207f0727175a7c79e4de67cb
Tom Ellis <tellishtc@gmail.com>
2016-02-17 07:50:37 +0000

    About page changes

--- src/TomahawkPHP/Bundle/WebBundle/Resources/views/About/home.php
+++ src/TomahawkPHP/Bundle/WebBundle/Resources/views/About/home.php
@@ -7,21 +7,15 @@
     </h1>
 
     <p>
-        I came up with the Tomahawk PHP framework about April 2014.
+       Development of Tomahawk PHP framework started in April 2014.
     </p>
 
     <p>
-        The framework we were using at the time was getting (in my opinion) a bit ridiculous. The way the
-        development was going I didn't agree with, picking and choosing when to use Semvar for instance,
-        which would introduce breaking changes in simple updates.
+        TomahawkPHP supports Semvar and where possble used libraries that already exist. Theres no point re-inventing the wheel right?
     </p>
 
     <p>
-        I wanted to build a framework that supported Semvar and that used the best of what was out there already.
-    </p>
-
-    <p>
-        For one Tomahawk is build upon Symfony2 components, specifically for handling requests and routing.
+        Tomahawk is built upon Symfony2 components, specifically for handling requests and routing.
         We also use Doctrine and Illuminate Database for the DB layers.
     </p>
 
@@ -29,6 +23,12 @@
         Other features have been build specifically for use in the Tomahawk PHP framework, but will become services
         at a later date.
     </p>
+    
+    <h2>Development Team</h2>
+    
+    <p>Currently the development team only consists of one person Tom Ellis.</p>
+    
+    <p>He is the Creator and Lead Developer of TomahawkPHP.</p>
 
 
 <?php $view['blocks']->stop() ?>
