12c244dea834fa9cd84272e6b8f2dd3af3391071
Tom Ellis <tellishtc@gmail.com>
2017-10-27 07:48:54 +0100

    Add google ad sense

--- src/TomahawkPHP/Bundle/WebBundle/Resources/views/Home/welcome.php
+++ src/TomahawkPHP/Bundle/WebBundle/Resources/views/Home/welcome.php
@@ -105,6 +105,17 @@
                     </ul>
                 </div>
             </div>
+            <div class="row">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Wide -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:728px;height:90px"
+                     data-ad-client="ca-pub-1530624277033637"
+                     data-ad-slot="8701754476"></ins>
+                <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
         </div>
 
     </div>
