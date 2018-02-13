abb4fd4d539c52147c4d31be577d4fbea55b6a5a
Tom Ellis <tellishtc@gmail.com>
2018-02-13 21:19:36 +0000

    Update dependencies

--- src/TomahawkPHP/Bundle/WebBundle/Controller/BaseController.php
+++ src/TomahawkPHP/Bundle/WebBundle/Controller/BaseController.php
@@ -18,7 +18,7 @@ class BaseController extends Controller
     /**
      * @var string
      */
-    protected $publishedVersion = '2.1.0';
+    protected $publishedVersion = '2.1.1';
 
     /**
      * @var array
